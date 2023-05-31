e6d0b7a5677f5ff3f0de84f0733b0158458f1595
lihao <15502354225@163.com>
2023-05-31 18:01:12 +0800

    add: 版本1.0.0 单个tabs轮询数据

--- src/background.ts
+++ src/background.ts
@@ -17,19 +17,17 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
 });
 
 function startGetUrlReferer(arr, index) {
+  let params = { url: arr[index], run: true };
   dataList = arr;
+
   if (!current?.run && serviceId) {
-    current = { url: arr[index], run: true };
-    chrome.tabs.update(serviceId, {
-      url: arr[index]
-    });
+    current = params;
+    chrome.tabs.update(serviceId, { url: arr[index] });
     currentIndex = index + 1;
     return;
   }
-  current = { url: arr[index], run: true };
-  chrome.tabs.create({
-    url: arr[index]
-  });
+  current = params;
+  chrome.tabs.create({ url: arr[index] });
   currentIndex = index + 1;
 }
 
@@ -37,31 +35,24 @@ chrome.tabs.onUpdated.addListener(function (tabId, info, tab) {
   if (!serviceId) {
     serviceId = tabId;
   }
-  if (serviceId !== tabId) {
-    return;
-  }
+  if (serviceId !== tabId) return;
   clearTimeout(tabTimer);
   tabTimer = null;
   if (!tabList[currentIndex]) {
     tabList[currentIndex] = [tab.url];
   } else {
-    if (tabList[currentIndex][tabList[currentIndex].length - 1] !== tab.url) {
-      tabList[currentIndex].push(tab.url);
-    }
+    if (tabList[currentIndex][tabList[currentIndex].length - 1] !== tab.url) tabList[currentIndex].push(tab.url);
   }
-  if (tab.status === 'complete') {
-    tabTimer = setTimeout(() => {
-      console.log(tab.url);
-      clearTimeout(tabTimer);
-      tabTimer = null;
-      current.run = false;
-      startGetUrlReferer(dataList, currentIndex);
-      if (dataList.length === Object.keys(tabList).length) {
-        clearData();
-      }
-      // });
-    }, 2000);
-  }
+  if (tab.status !== 'complete') return;
+  tabTimer = setTimeout(() => {
+    console.log(tab.url);
+    clearTimeout(tabTimer);
+    tabTimer = null;
+    current.run = false;
+    startGetUrlReferer(dataList, currentIndex);
+    if (dataList.length !== Object.keys(tabList).length) return;
+    clearData();
+  }, 2000);
 });
 
 function clearData() {
--- src/content/bindCurrent.ts
+++ src/content/bindCurrent.ts
@@ -2,7 +2,7 @@ import { TAB_INFO } from '../config/types';
 
 export const openNewTab = (data) => {
   chrome.runtime.sendMessage({ tab: TAB_INFO, data }, (result) => {
-    console.log(result, '发送新tag成功');
+    console.log(result, 'res');
   });
 };
 
